0bc7cc36ecf4cbff8a412a7f62e4c4e41d51c97f
Juan Miguel Manalo <jm.cruz.manalo@gmail.com>
2019-02-18 16:45:32 +0800

    Initial base

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
   }
-}
+
+  html, body {
+    height: 100%;
+    width: 100%;
+    position: relative;    
+    margin: 0;
+    padding: 0;
+  }
+
+  #root {
+    display: inline;
+  }
+`;
+
+const RootDiv = styled('div')`
+  display: inline;
+`;
+
+const App = () => (
+  <RootDiv>
+    <GlobalStyle />
+    <h1>React-Spring</h1>
+  </RootDiv>
+);
 
 export default App;
